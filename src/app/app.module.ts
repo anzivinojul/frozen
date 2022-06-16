af3aaeb8656bd26a0d865d3592286d1a78fb674b
anzivinojul <anzivinojulien@gmail.com>
2022-06-16 23:37:27 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { HeaderComponent } from './templates/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/main/main.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartDataset } from 'chart.js';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+
+  temperatureHistoryData: ChartDataset[] = [
+
+    { data: [22,23,23,24,24,26,26,27,26,25,24,22,20], label: 'Température' },
+
+  ];
+
+  temperatureHistoryLabels: string[] = [];
+
+  temperatureHistoryOptions = {
+    responsive: true,
+  }
+
+  temperatureHistoryLegends = false;
+
+  constructor() { }
+
+  fillHoursHistory() {
+    const dateToday = new Date();
+    let actualHour = dateToday.getHours();
+    let actualHourString = '';
+
+    for(let i = 0 ; i <= 10 ; i++) {
+
+      if(actualHour == 0) {
+        actualHour = 23;
+        actualHourString = actualHour.toString();
+      }
+      else if(actualHour < 11) {
+        actualHour--;
+        actualHourString = actualHour.toString();
+      }
+      else {
+        actualHour--;
+        actualHourString = actualHour.toString();
+      }
+
+      this.temperatureHistoryLabels.push(actualHourString + 'h');
+    }
+
+    this.temperatureHistoryLabels.reverse();
+  }
+
+  ngOnInit(): void {
+
+    this.fillHoursHistory();
+  }
+
+}
